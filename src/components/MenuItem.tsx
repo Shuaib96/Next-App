580b15452d5911fa0c1c4ee45c8d06a0aaea1377
Shuaib96 <94510688+Shuaib96@users.noreply.github.com>
2025-12-11 15:53:12 +0530

    create next app

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import type { IconType } from "react-icons";
+
+interface MenuItemProps {
+  title: string;
+  address: string;
+  Icon: IconType;
+}
+
+export default function MenuItem({ title, address, Icon }: MenuItemProps) {
+  return (
+    <Link href={address} className="hover:text-amber-500">
+      <Icon className="text-2xl sm:hidden" />
+      <p className="uppercase hidden sm:inline text-sm">{title}</p>
+    </Link>
+  );
+}
