6c572afbad7eecb2dc0a62927ee3f0f30beb186f
Shuaib96 <94510688+Shuaib96@users.noreply.github.com>
2025-12-12 20:33:38 +0530

    added search page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Providers from "./Providers";
 import NavBar from "@/components/NavBar/NavBar";
+import SearchBox from "@/components/SearchBox/SearchBox";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Providers>
           <Header />
           <NavBar />
+          <SearchBox />
           {children}
         </Providers>
       </body>
